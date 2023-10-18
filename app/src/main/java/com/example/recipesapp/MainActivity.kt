07aeb764447c9ea035c773a42545c50a0b4c5fd6
bishal5917 <poudelb172@gmail.com>
2023-10-18 16:25:56 +0545

    google login with google onetap success

--- app/src/main/java/com/example/recipesapp/MainActivity.kt
+++ app/src/main/java/com/example/recipesapp/MainActivity.kt
@@ -1,39 +1,109 @@
 package com.example.recipesapp
 
+import android.content.IntentSender
 import android.os.Bundle
-import androidx.activity.viewModels
+import android.util.Log
+import android.widget.Button
+import androidx.activity.result.IntentSenderRequest
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.viewModels
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.CommonStatusCodes
 import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.example.recipesapp.features.presentation.recipes.viewmodel.RecipeEvent
-import com.example.recipesapp.features.presentation.recipes.viewmodel.RecipeViewModel
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.gms.auth.api.identity.Identity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private var TAG: String = "GOOGLE";
+    private var oneTapClient: SignInClient? = null
+    private var signUpRequest: BeginSignInRequest? = null
+    private var signInRequest: BeginSignInRequest? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        navController = navHostFragment.navController
+        oneTapClient = Identity.getSignInClient(this)
+        signUpRequest = BeginSignInRequest.builder().setGoogleIdTokenRequestOptions(
+            BeginSignInRequest.GoogleIdTokenRequestOptions.builder().setSupported(true)
+                // Your server's client ID, not your Android client ID.
+                .setServerClientId(getString(R.string.web_client_id))
+                // Show all accounts on the device.
+                .setFilterByAuthorizedAccounts(false).build()
+        ).build()
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        bottomNavigationView.setupWithNavController(navController)
+        val btn = findViewById<Button>(R.id.btnGoogleSignIn)
+
+        btn.setOnClickListener {
+            Log.d(TAG, "I am clicked")
+            displaySignUp()
+        }
+
+//        val navHostFragment =
+//            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+//        navController = navHostFragment.navController
+
+//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+//        bottomNavigationView.setupWithNavController(navController)
 
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+    //    override fun onSupportNavigateUp(): Boolean {
+//        return navController.navigateUp() || super.onSupportNavigateUp()
+//    }
+    private val oneTapResult =
+        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
+            try {
+                val credential = oneTapClient?.getSignInCredentialFromIntent(result.data)
+                val idToken = credential?.googleIdToken
+                when {
+                    idToken != null -> {
+                        // Got an ID token from Google. Use it to authenticate
+                        // with your backend.
+                        val msg = "idToken: $idToken"
+                        Log.d(TAG, msg)
+                    }
+
+                    else -> {
+                        // Shouldn't happen.
+                        Log.d(TAG, "No ID token!")
+                    }
+                }
+            } catch (e: ApiException) {
+                when (e.statusCode) {
+                    CommonStatusCodes.CANCELED -> {
+                        Log.d(TAG, "One-tap dialog was closed.")
+                    }
+
+                    CommonStatusCodes.NETWORK_ERROR -> {
+                        Log.d(TAG, "One-tap encountered a network error.")
+                    }
+
+                    else -> {
+                        Log.d(
+                            TAG, "Couldn't get credential from result." + " (${e.localizedMessage})"
+                        )
+                    }
+                }
+            }
+        }
+
+    private fun displaySignUp() {
+        oneTapClient?.beginSignIn(signUpRequest!!)?.addOnSuccessListener(this) { result ->
+            try {
+                val ib = IntentSenderRequest.Builder(result.pendingIntent.intentSender).build()
+                oneTapResult.launch(ib)
+            } catch (e: IntentSender.SendIntentException) {
+                Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+            }
+        }?.addOnFailureListener(this) { e ->
+            // No Google Accounts found. Just continue presenting the signed-out UI.
+            Log.d(TAG, e.localizedMessage!!)
+        }
     }
 }
